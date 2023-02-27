102b1caa065a63ed30aea168fa354c76da0819f2
Samuel Labastille <samuellabastille17@gmail.com>
2023-02-27 12:30:32 -0500

    basics work

---
+++ src/popup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import Axios from 'axios'
+
+
+
+function Popup() {
+    return (<div>Hello world</div>)
+}
+render(<Popup/>, document.getElementById("root") )
+
